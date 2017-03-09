8df1f3106dc0a973e19d3b3ae73d67e7a5313776
René Winkelmeyer <mail@winkelmeyer.com>
2017-03-09 08:39:19 +0100

    Added Pardot IDs

--- sparkling-theme-customizations/footer.php
+++ sparkling-theme-customizations/footer.php
@@ -37,6 +37,19 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script type="text/javascript">
+piAId = '28572';
+piCId = '82695';
 
+(function() {
+    function async_load(){
+        var s = document.createElement('script'); s.type = 'text/javascript';
+        s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+    }
+    if(window.attachEvent) { window.attachEvent('onload', async_load); }
+    else { window.addEventListener('load', async_load, false); }
+})();
+</script>
 </body>
 </html>
